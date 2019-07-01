41f59d92a33ca9207d4732734f160480ed8ec921
Michael Shields <michael.shields@freenome.com>
2019-07-01 12:53:20 -0700

    Allow provision.sh to be run as ./provision.sh.
    $(dirname "$0") is "." in that case, which doesn't work out.

--- provision.sh
+++ provision.sh
@@ -2,4 +2,5 @@
 
 set -e
 
-(cd $(dirname "$0") && tar cf - $(find . -type f | grep -v -e '^\./\.git/' -e '^\./[^.]')) | tar xvf -
+tar cf - $(find . -type f | grep -v -e '^\./\.git/' -e '^\./[^.]') \
+    | (cd "$HOME" && tar xvf -)
