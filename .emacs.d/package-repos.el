2be278e17a243dd00ac077b391a963741c001069
Michael Shields <michael.shields@freenome.com>
2020-04-16 19:53:38 -0700

    Fresher flycheck

--- .emacs.d/package-repos.el
+++ .emacs.d/package-repos.el
@@ -4,6 +4,15 @@
 ;; recklessly uses HTTP/80.
 (setq package-archives
       '(("gnu" . "https://elpa.gnu.org/packages/")
+	("melpa" . "https://melpa.org/packages/")
 	("melpa-stable" . "https://stable.melpa.org/packages/")))
+
+(setq package-archive-priorities
+      '(("gnu" . 50)
+	("melpa" . 0)
+	("melpa-stable" . 100)))
+
+(setq package-pinned-packages
+      '((flycheck . "melpa")))
 
 (package-initialize)
