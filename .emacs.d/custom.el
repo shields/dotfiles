d094878fb8de92cc6c7f7e8e37e07da7b6949296
Michael Shields <michael.shields@freenome.com>
2020-04-16 19:02:42 -0700

    Adjust faces

--- .emacs.d/custom.el
+++ .emacs.d/custom.el
@@ -1,9 +1,19 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (idle-highlight eterm-256color minions doom-modeline all-the-icons ergoemacs-status ergoemacs-modeline yasnippet spaceline smartparens projectile multi-term markdown-mode magit go-eldoc flycheck flx-ido exec-path-from-shell company-go aggressive-indent))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(default ((t (:inherit nil :stipple nil :background "White" :foreground "Black" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 120 :width normal :foundry "nil" :family "Andale Mono"))))
+ '(cperl-array-face ((t (:foreground "Blue"))))
+ '(cperl-hash-face ((t (:foreground "Red" :weight bold))))
  '(cursor ((t (:background "firebrick"))))
  '(doom-modeline-bar ((t nil)))
  '(doom-modeline-buffer-file ((t (:inherit mode-line-buffer-id))))
@@ -11,7 +21,7 @@
  '(doom-modeline-buffer-path ((t (:inherit mode-line-emphasis))))
  '(doom-modeline-project-dir ((t nil)))
  '(fixed-pitch ((t nil)))
- '(minibuffer-prompt ((t (:foreground "medium blue" :weight semi-bold :family "Avenir Next"))))
+ '(minibuffer-prompt ((t (:weight bold))))
  '(mode-line ((t (:background "grey90" :foreground "black" :box (:line-width -1 :style released-button) :family "Avenir Next"))))
  '(mode-line-buffer-id ((t (:weight semi-bold))))
  '(mode-line-emphasis ((t (:weight semi-bold))))
