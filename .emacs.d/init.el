eed0372ca945c678e3c7a8bdfc4e7089234aff5a
Michael Shields <michael.shields@freenome.com>
2020-04-17 11:29:45 -0700

    ora-complete-number

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -315,6 +315,24 @@ when called with a prefix argument."
 (setq company-tooltip-align-annotations t)
 (setq company-tooltip-flip-when-above t)
 
+;; https://oremacs.com/2017/12/27/company-numbers/
+(defun ora-company-number ()
+  "Forward to `company-complete-number'.
+
+Unless the number is potentially part of the candidate.
+In that case, insert the number."
+  (interactive)
+  (let* ((k (this-command-keys))
+         (re (concat "^" company-prefix k)))
+    (if (cl-find-if (lambda (s) (string-match re s))
+                    company-candidates)
+        (self-insert-command 1)
+      (company-complete-number (string-to-number k)))))
+(mapc
+ (lambda (x)
+   (define-key company-active-map (format "%d" x) #'ora-company-number))
+ (number-sequence 0 9))
+
 ;;}}}
 ;;{{{ Flycheck
 
