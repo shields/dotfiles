8c0d446e7b5870293dfb92c967c06cd1aa29297d
Michael Shields <michael.shields@freenome.com>
2020-04-16 18:27:19 -0700

    Faces

--- .emacs.d/custom.el
+++ .emacs.d/custom.el
@@ -5,7 +5,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (minions doom-modeline all-the-icons ergoemacs-status ergoemacs-modeline yasnippet spaceline smartparens projectile multi-term markdown-mode magit go-eldoc flycheck flx-ido exec-path-from-shell company-go aggressive-indent))))
+    (idle-highlight eterm-256color minions doom-modeline all-the-icons ergoemacs-status ergoemacs-modeline yasnippet spaceline smartparens projectile multi-term markdown-mode magit go-eldoc flycheck flx-ido exec-path-from-shell company-go aggressive-indent))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -19,7 +19,10 @@
  '(doom-modeline-buffer-path ((t (:inherit mode-line-emphasis))))
  '(doom-modeline-project-dir ((t nil)))
  '(fixed-pitch ((t nil)))
+ '(minibuffer-prompt ((t (:foreground "medium blue" :weight semi-bold :family "Avenir Next"))))
  '(mode-line ((t (:background "grey90" :foreground "black" :box (:line-width -1 :style released-button) :family "Avenir Next"))))
  '(mode-line-buffer-id ((t (:weight semi-bold))))
  '(mode-line-emphasis ((t (:weight semi-bold))))
- '(mode-line-inactive ((t (:inherit mode-line :background "grey80" :foreground "grey20" :box (:line-width -1 :color "grey75") :weight light)))))
+ '(mode-line-inactive ((t (:inherit mode-line :background "grey80" :foreground "grey20" :box (:line-width -1 :color "grey75") :weight light))))
+ '(show-paren-match ((t (:foreground "magenta" :weight bold))))
+ '(show-paren-match-expression ((t (:background "#fff6ff")))))
