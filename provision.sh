4861d93f6acf8b011403ffebcb1841668cc5d577
Michael Shields <michael.shields@freenome.com>
2020-04-18 15:55:49 -0700

    Conditionally set home or work email

--- provision.sh
+++ provision.sh
@@ -4,3 +4,11 @@ set -e
 
 tar cf - $(find . -type f | grep -v -e '^\./\.git/' -e '^\./[^.]') \
     | (cd "$HOME" && tar xvf -)
+
+# Is this a Freenome-managed Mac?
+if [[ "$(uname)" == Darwin ]] && \
+       system_profiler SPConfigurationProfileDataType | grep -qi freenome; then
+    git config --global user.email michael.shields@freenome.com
+else
+    git config --global user.email shields@msrl.com
+fi
