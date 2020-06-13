c5b90c6dac843a5e6e51f6916a74a2a9617a9d6e
Michael Shields <michael.shields@freenome.com>
2020-06-12 18:15:27 -0700

    Revert "jsonnet auto reformat"
    jsonnet-mode doesn't handle errors well enough to use this.
    
    This reverts commit 5af04fa9aeab7d80636131e23e00f8caa32a96ef.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -398,14 +398,6 @@ stage it and display a diff."
 (define-key help-mode-map [(q)] #'previous-buffer)
 
 ;;}}}
-;;{{{ jsonnet-mode
-
-(defun shields/jsonnet-install-save-hook ()
-  (add-hook 'before-save-hook #'jsonnet-reformat-buffer t t))
-(eval-after-load "jsonnet"
-  (add-hook 'jsonnet-mode-hook #'shields/jsonnet-install-save-hook))
-
-;;}}}
 ;;{{{ makefile-mode
 
 (add-hook 'makefile-mode-hook
