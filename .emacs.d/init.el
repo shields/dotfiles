79d90181c8f4c3d6ab87b55ee63a09da8a4a4a37
Michael Shields <michael.shields@freenome.com>
2020-06-12 18:09:06 -0700

    Fix for aggressive-indent bug

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -20,6 +20,23 @@
 ;; https://github.com/mgyucht/jsonnet-mode/pull/16
 (add-to-list 'auto-mode-alist (cons "\\.libsonnet\\'" 'jsonnet-mode))
 
+;; https://github.com/Malabarba/aggressive-indent-mode/issues/137
+;; Closed 2020-05-12 but not fixed as requested.
+(require 'aggressive-indent)
+(defun aggressive-indent--indent-if-changed (buffer)
+  "Indent any region that changed in BUFFER in the last command loop."
+  (if (not (buffer-live-p buffer))
+      (and aggressive-indent--idle-timer
+           (cancel-timer aggressive-indent--idle-timer))
+    (with-current-buffer buffer
+      (when (and aggressive-indent-mode aggressive-indent--changed-list)
+        (save-excursion
+          (save-selected-window
+            (aggressive-indent--while-no-input
+              (aggressive-indent--proccess-changed-list-and-indent))))
+        (when (timerp aggressive-indent--idle-timer)
+          (cancel-timer aggressive-indent--idle-timer))))))
+
 ;;}}}
 ;;{{{ Customization of commands
 
