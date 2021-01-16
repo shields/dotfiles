82d2b06d016861a5feb49eb0f076868d42dc838d
Michael Shields <michael.shields@freenome.com>
2021-01-15 17:00:22 -0800

    Larger Emacs fonts

--- .emacs.d/custom.el
+++ .emacs.d/custom.el
@@ -10,7 +10,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :extend nil :stipple nil :background "White" :foreground "Black" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 100 :width normal :foundry "nil" :family "Andale Mono"))))
+ '(default ((t (:inherit nil :extend nil :stipple nil :background "White" :foreground "Black" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 120 :width normal :foundry "nil" :family "Andale Mono"))))
  '(Info-quoted ((t (:inherit fixed-pitch))))
  '(company-preview ((t (:foreground "gray60"))))
  '(cperl-array-face ((t (:foreground "Blue"))))
