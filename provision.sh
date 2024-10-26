ea3f97691ade3cf853332a259cac1f883cff4cd5
Michael Shields <shields@msrl.com>
2024-10-25 22:53:17 -0700

    Avoid spurious failure due to exit status of grep

--- provision.sh
+++ provision.sh
@@ -117,7 +117,7 @@ if [[ "$(dscl . read /Users/$(whoami) UserShell)" == "UserShell: /bin/zsh" ]]; t
 fi
 
 # Plugins!
-llm install --upgrade llm-{ollama,gemini,claude-3,perplexity,cmd} | grep -v 'Requirement already satisfied:'
+llm install --upgrade llm-{ollama,gemini,claude-3,perplexity,cmd}
 
 # Make sure System Preferences isn't open, since it interferes with other
 # processes writing to defaults.
