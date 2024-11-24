b08c32f00bd5de4dba6a8561051d04f6a18365d2
Michael Shields <shields@msrl.com>
2024-11-24 13:20:09 -0800

    I suppose I can't have three-finger drag and three-finger nav

--- provision.sh
+++ provision.sh
@@ -116,15 +116,18 @@ defaults write NSGlobalDomain 'com.apple.trackpad.forceClick' -bool false
 # Scroll down using the flexors of the fingers, which are stronger
 # than the extensors.
 defaults write NSGlobalDomain com.apple.swipescrolldirection -bool false
-# Three-finger drag and four-finger swipe between fullscreen apps
+# Three-finger trackpad drag
+defaults write com.apple.AppleMultitouchTrackpad Dragging true
 defaults write com.apple.AppleMultitouchTrackpad TrackpadThreeFingerDrag -bool true
+defaults write com.apple.AppleMultitouchTrackpad TrackpadThreeFingerHorizSwipeGesture -int 0
+defaults write com.apple.AppleMultitouchTrackpad TrackpadThreeFingerVertSwipeGesture -int 0
 defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad TrackpadThreeFingerDrag -bool true
-defaults write com.apple.AppleMultitouchTrackpad TrackpadFourFingerHorizSwipeGesture -int 2
-defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad TrackpadThreeFingerHorizSwipeGesture -int 1
-defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad TrackpadThreeFingerVertSwipeGesture -int 1
+defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad TrackpadThreeFingerHorizSwipeGesture -int 0
+defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad TrackpadThreeFingerVertSwipeGesture -int 0
 defaults write NSGlobalDomain AppleEnableSwipeNavigateWithScrolls false
-defaults write com.apple.AppleMultitouchTrackpad TrackpadThreeFingerHorizSwipeGesture -int 1
-defaults write com.apple.AppleMultitouchTrackpad TrackpadThreeFingerVertSwipeGesture -int 1
+# Four-finger swipe between fullscreen apps
+defaults write com.apple.AppleMultitouchTrackpad TrackpadFourFingerHorizSwipeGesture -int 2
+defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad TrackpadFourFingerHorizSwipeGesture -int 2
 # Tracking speed
 defaults write NSGlobalDomain 'com.apple.trackpad.scaling' -float 0.875
 
