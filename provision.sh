0965fcafec04343210ff04879f1e13b3326256d3
Michael Shields <shields@msrl.com>
2025-04-17 17:51:11 -0700

    Set a lighter blue for AppleHighlightColor

--- provision.sh
+++ provision.sh
@@ -91,6 +91,9 @@ defaults write NSGlobalDomain AppleShowScrollBars -string 'Always'
 defaults write NSGlobalDomain NSAutomaticWindowAnimationsEnabled -bool false
 defaults write NSGlobalDomain NSScrollAnimationEnabled -bool false
 defaults write NSGlobalDomain NSWindowResizeTime -float 0.001
+# A lighter blue than standard '0.698039 0.843137 1.000000 Blue', which
+# is OKLCH (0.8633, 0.0695, 251.25). I think this is in linear RGB.
+defaults write NSGlobalDomain AppleHighlightColor '0.56153 0.75605 1.01381 Other'
 # Avoid having to sudo by checking this value before writing it. For some
 # reason, 1.5 is stored as 1.496458570615034, so compare it as a number.
 if [[ $(bc -e "abs($(defaults read com.apple.universalaccess mouseDriverCursorSize) - 1.5) < 0.01") != 1 ]]; then
