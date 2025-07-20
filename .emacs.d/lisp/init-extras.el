17af4f461ab244bee5df193e515feefcf536a611
Michael Shields <shields@msrl.com>
2025-07-19 23:39:13 -0700

    Remove fff

--- .emacs.d/lisp/init-extras.el
+++ .emacs.d/lisp/init-extras.el
@@ -10,19 +10,7 @@
 ;; Misc tooling
 (autoload 'rfcview-mode "rfcview")
 
-(eval-after-load "fff"
-  '(progn
-     (setq fff-map-prefix [(control c) (f)])
-     (fff-install-map)
-     (require 'fff-rfc)
-     (fff-rfc-install-map)
-     (setq fff-rfc-view-mode 'rfcview-mode)
-     ;; Debian doc-rfc-* packages use this directory:
-     (add-to-list 'fff-rfc-path "/usr/share/doc/RFC/links")
-     (require 'fff-elisp)
-     (fff-elisp-install-map)))
-(unless (featurep 'fff)
-  (load "fff" t))
+
 
 (provide 'init-extras)
 ;;; init-extras.el ends here
